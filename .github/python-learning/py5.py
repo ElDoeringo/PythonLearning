40f44d159a081016961727cca9f9eb4c5a5a9c6d
ElDoeringo <thdj@thetechcollective.eu>
2024-05-21 07:50:06 +0000

    jeg lærte X

---
+++ .github/python-learning/py5.py
@@ -0,0 +1,17 @@
+x = 3 #Video 5 Betinget udtryk i Python
+
+if x >2:
+    print("x er større end to")
+elif x ==2:
+    print("x er lig med to")
+else:
+    print("x er mindre end to")
+
+x = 3
+y = 4
+if x == 3 and y == 4:
+    print("x er lig med tre og y er lig med 4")
+if x == 3 or y == 4:
+     print("x er lig med tre eller y er lig med 4")
+if x != 3:
+     print("x er ikke lig med tre")
