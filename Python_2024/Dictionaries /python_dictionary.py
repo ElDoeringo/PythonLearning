2788f881e189833c2da2b3ed964f9cbd07d2b9e0
ElDoeringo <thdj@thetechcollective.eu>
2024-05-22 09:00:24 +0000

    new

--- Python_2024/Dictionaries /python_dictionary.py
+++ Python_2024/Dictionaries /python_dictionary.py
@@ -1,1 +1,8 @@
 #Def - en dictionary er en liste af objekter med en reference
+#Dictionary en liste af nøgleværdipar
+#Hvordan erklæres en dictionary?
+#   dict = {"key0" : elem0, "key1" : elem1} 
+i = ("hund")
+A = ("hund, kat, hamster, frø")
+
+print(A)
