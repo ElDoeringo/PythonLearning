61176a13fd4c038a3af3a9d162ae89e7df3c376f
Thomas Doering Jensen Thiel <thdj@thetechcollective.eu>
2024-06-17 11:24:58 +0000

    Commented the PieChart script according step/action

--- Python_2024/Pakker i Python/Pakker/PieChart.py
+++ Python_2024/Pakker i Python/Pakker/PieChart.py
@@ -1,7 +1,7 @@
 #Cirkel diagram
 import matplotlib.pyplot as plt  # Import the pyplot module from matplotlib
 import numpy as np  # Import the NumPy library
-name = ['Dog', 'Cat', 'Pig', 'Horse']
-number = [10, 20, 30, 40]
-plt.pie(number, labels=name)
-plt.show()
+name = ['Dog', 'Cat', 'Pig', 'Horse'] # Names of the categories in the pie chart
+number = [10, 20, 30, 40] # Data for each category (make sure the numbers are correct)
+plt.pie(number, labels=name) # Plot the pie chart with the provided data and labels
+plt.show() # Display the pie chart
