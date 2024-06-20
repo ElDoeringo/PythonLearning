c1602737f324001e500e32a50bf2273a08bf55ca
Thomas Doering Jensen Thiel <thdj@thetechcollective.eu>
2024-06-20 07:24:28 +0000

    Update of webscraping script using beautiful soup

--- Python_2024/Parse_Webscraping/parse.py
+++ Python_2024/Parse_Webscraping/parse.py
@@ -1,9 +1,18 @@
-import urllib.request as urllibR
+import urllib.request as urllibR  # Import the urllib.request module and alias it as urllibR for handling URL requests
+from bs4 import BeautifulSoup  # Import BeautifulSoup from the bs4 library for parsing HTML
 
+# Define the URL to be accessed
+url = 'https://tv2.dk/'
 
-url ='https://tv2.dk/'
+# Open the URL and read the response
+tv2 = urllibR.urlopen(url)  # Open the URL and get the response object
 
-tv2BS = bs4.BeautifulSoup(tv2.read())
-tv2 = urllibR.urlopen(url)
+# Read the HTML content from the response
+html_content = tv2.read()  # Read the HTML content from the response object
 
-print(tv2.read())
+# Parse the HTML content using BeautifulSoup
+tv2BS = BeautifulSoup(html_content, 'html.parser')  # Parse the HTML content with BeautifulSoup
+
+# Print the parsed HTML content
+print(tv2BS.prettify())  # Print the parsed HTML content in a prettified format
+
