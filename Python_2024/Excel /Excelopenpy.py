5e304bd4048321adccadf71c18ef9e086b0a155f
Thomas Doering Jensen Thiel <thdj@thetechcollective.eu>
2024-07-04 07:35:04 +0000

    Updated Excel

--- Python_2024/Excel /Excelopenpy.py
+++
@@ -1,4 +0,0 @@
-import openpyxl as xl  # Import the openpyxl library as xl for working with Excel files
-
-wb = xl.load_workbook('/this PC/Desktop/Timeseddel.xlsx')  # Load the workbook named 'Timeseddel.xlsx'
-print(wb.sheetnames) # Print the names of all the sheets in the workbook
