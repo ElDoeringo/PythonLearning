ade85a272e4c24028386a1c36220b8726e6a669e
Thomas Doering Jensen Thiel <thdj@thetechcollective.eu>
2024-07-04 07:21:25 +0000

    New Python print script

---
+++ Python_2024/Print/Python_Print4.py
@@ -0,0 +1,4 @@
+fruits = ["banana", "apple", "cherry"]
+sorted_fruits = sorted(fruits)  # Creates a new sorted list
+print(sorted_fruits)  # Prints: ['apple', 'banana', 'cherry']
+print(fruits)  # Original list remains unchanged
